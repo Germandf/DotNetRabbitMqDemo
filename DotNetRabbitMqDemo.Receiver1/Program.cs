7ee2f86b6e18b7a1f637a18dfa549e0402b716b7
germandefrancesco <germandefrancesco@hotmail.com>
2023-03-06 13:24:22 -0300

    Initial commit

---
+++ DotNetRabbitMqDemo.Receiver1/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Receiver1 App";
+
+var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+var exchangeName = "DemoExchange";
+var routingKey = "demo-routing-key";
+var queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+channel.BasicQos(0, 1, false);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, args) =>
+{
+    Task.Delay(TimeSpan.FromSeconds(5)).Wait();
+
+    var body = args.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"Message Received: {message}");
+
+    channel.BasicAck(args.DeliveryTag, false);
+};
+
+var consumerTag = channel.BasicConsume(queueName, false, consumer);
+
+Console.ReadLine();
+
+channel.BasicCancel(consumerTag);
+
+channel.Close();
+connection.Close();
---
+++ DotNetRabbitMqDemo.Sender/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Sender App";
+
+var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+var exchangeName = "DemoExchange";
+var routingKey = "demo-routing-key";
+var queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+for (int i = 0; i < 60; i++)
+{
+    Console.WriteLine($"Sending Message {i}");
+    var messageBodyBytes = Encoding.UTF8.GetBytes($"Message #{i}");
+    channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+    Thread.Sleep(1000);
+}
+
+channel.Close();
+connection.Close();
