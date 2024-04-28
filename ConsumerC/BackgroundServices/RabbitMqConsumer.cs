51de335a12da8156cb905c10fad2c4d8521092e7
Germán De Francesco <germandefrancesco@hotmail.com>
2024-04-28 12:49:37 -0300

    Add RabbitMqConsumer BackgroundService in ConsumerC project

---
+++ ConsumerC/BackgroundServices/RabbitMqConsumer.cs
@@ -0,0 +1,79 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace ConsumerC.BackgroundServices;
+
+public class RabbitMqConsumer : BackgroundService
+{
+    private readonly IConnection _connection;
+    private readonly IModel _channel;
+    private ILogger<RabbitMqConsumer> _logger;
+
+    public RabbitMqConsumer(ILogger<RabbitMqConsumer> logger)
+    {
+        _logger = logger;
+
+        var factory = new ConnectionFactory
+        {
+            HostName = "localhost",
+            UserName = "guest",
+            Password = "guest",
+            VirtualHost = "/",
+            // https://www.rabbitmq.com/client-libraries/dotnet-api-guide#consuming-async
+            DispatchConsumersAsync = true,
+        };
+
+        _connection = factory.CreateConnection();
+
+        _channel = _connection.CreateModel();
+
+        _channel.ExchangeDeclare(
+            "dotnet.rabbitmq.demo",
+            ExchangeType.Topic,
+            durable: true,
+            autoDelete: false);
+
+        _channel.QueueDeclare(
+            "dotnet.rabbitmq.demo.consumer.c",
+            durable: true,
+            exclusive: false,
+            autoDelete: false);
+
+        _channel.QueueBind(
+            "dotnet.rabbitmq.demo.consumer.c",
+            "dotnet.rabbitmq.demo",
+            "*.*");
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        stoppingToken.ThrowIfCancellationRequested();
+
+        var consumer = new AsyncEventingBasicConsumer(_channel);
+
+        consumer.Received += async (model, args) =>
+        {
+            await Task.Delay(500);
+            var body = args.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            try
+            {
+                if (message.Contains("test"))
+                    throw new Exception("test");
+
+                _logger.LogInformation($"{args.RoutingKey}: {message}");
+                _channel.BasicAck(args.DeliveryTag, false);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation($"Error processing message: {ex.Message}");
+                _channel.BasicNack(args.DeliveryTag, multiple: false, requeue: true);
+            }
+        };
+
+        _channel.BasicConsume("dotnet.rabbitmq.demo.consumer.c", false, consumer);
+
+        return Task.CompletedTask;
+    }
+}
--- ConsumerC/Program.cs
+++ ConsumerC/Program.cs
@@ -1,8 +1,11 @@
+using ConsumerC.BackgroundServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHostedService<RabbitMqConsumer>();
 
 var app = builder.Build();
 
